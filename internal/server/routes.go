52e42e7a760a9a772edde3e83881332e33564eae
Miles Stripling <121267075+mstripling@users.noreply.github.com>
2024-11-18 12:39:37 -0800

    Fixed formatting and documentation

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,22 +1,22 @@
 package server
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
-  "fmt"
-  "hopp/internal/util"
+    "encoding/json"
+    "log"
+    "net/http"
+    "fmt"
+    "hopp/internal/util"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", s.HelloWorldHandler)
-  mux.HandleFunc("/json", s.HashHandler)
-
-	mux.HandleFunc("/health", s.healthHandler)
-
-	return mux
+    
+    mux := http.NewServeMux()
+    mux.HandleFunc("/", s.HelloWorldHandler)
+    mux.HandleFunc("/json", s.HashHandler)
+    
+    mux.HandleFunc("/health", s.healthHandler)
+    
+    return mux
 }
 
 func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +24,8 @@ func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
 	resp["message"] = "Hello World"
 
 	jsonResp, err := json.Marshal(resp)
-	if err != nil {
+	
+    if err != nil {
 		log.Fatalf("error handling JSON marshal. Err: %v", err)
 	}
 
@@ -42,7 +43,6 @@ func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) HashHandler(w http.ResponseWriter, r *http.Request) {
- // Only accept POST requests
     if r.Method != http.MethodPost {
         http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
         return
@@ -50,32 +50,31 @@ func (s *Server) HashHandler(w http.ResponseWriter, r *http.Request) {
 
     // Decode the JSON request body into the struct
     var payload util.Payload
-
-    err := json.NewDecoder(r.Body).Decode(&payload)
+    err := json.NewDecoder(r.Body).Decode(&payload) //Make this easier to read?
     if err != nil {
         http.Error(w, fmt.Sprintf("Invalid JSON: %s", err), http.StatusBadRequest)
         return 
     }
+    
+    //Initiliaze blank object if doesn't exist
     if payload.Plain == nil {
       payload.Plain = make(map[string]interface{})
     }
-    
     if payload.Hash == nil {
       payload.Hash = make(map[string]interface{})
     }
 
-  // Now call the HashFunction to process the data
+    // Call the Hash function to process the data
 	processedPayload, err := util.Hash(payload)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error processing data: %s", err), http.StatusInternalServerError)
 		return
 	}
 
-	// Return the merged data as a single JSON object
+	// Return the merged data
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(processedPayload); err != nil {
 		http.Error(w, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
 		return
 	}
 }
-
