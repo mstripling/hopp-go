0fea55880d6f173d0ccb15f1a71cd35f16b9d034
Miles Stripling <121267075+mstripling@users.noreply.github.com>
2024-11-25 08:58:44 -0800

    formatting and documentation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-  "net/http"
+	"net/http"
 	"os/signal"
 	"syscall"
 	"time"
@@ -15,14 +15,16 @@ import (
 func gracefulShutdown(apiServer *http.Server, done chan bool) {
 	// Create context that listens for the interrupt signal from OS.
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+	// Executes cancelFunc once we reach end of scope of the function
 	defer stop()
 
 	// Listen for the interrupt signal.
 	<-ctx.Done()
 
 	log.Println("shutting down gracefully, press Ctrl+C again to force")
-
-    // Server has 5 seconds to finish last request before quitting
+	
+	// Server has 5 seconds to finish last request before quitting
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	if err := apiServer.Shutdown(ctx); err != nil {
