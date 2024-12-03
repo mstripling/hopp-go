b2b1f5e2a3118d6abd4e0a65a70dfacf3ca10328
Miles Stripling <miles.stripling@gmail.com>
2024-12-03 12:05:51 -0800

    removed files

--- generate-init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load environment variables
-if [ -f .env ]; then
-  export $(grep -v '^#' .env | xargs)
-fi
-
-envsubst < mysql-init/init.template.sql > mysql-init/init.sql
--- post_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define the URL and file path
-URL="http://localhost:8080/ping"
-FILE_PATH="internal/util/vendor.json"
-
-# Send the POST request
-curl -X POST -H "Content-Type: application/json" -d @$FILE_PATH $URL
